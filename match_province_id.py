e2d00e5c58768240b94a9844249239180409db89
Spirit <drmelown@gmail.com>
2022-10-29 15:20:21 +0600

    adding definition and position files

--- match_province_id.py
+++ match_province_id.py
@@ -40,6 +40,38 @@ def natural_sort(l):
     return sorted(l, key=alphanum_key)
 
 
+def definition_file():
+    province_id = []
+    definition_id = []
+    comment = '#'
+    province = "province;red;green;blue;x;x"
+    semicolon = ";"
+    path = definition_file_path
+    with open(path) as file:
+        while line := file.readline().rstrip().replace('\t', '').replace('\n', '').replace(' ', ''):
+            line = line.split(comment, 1)[0].replace(province, "").removesuffix(";")
+            id_line = line.split(semicolon, 1)[0].split('\n')
+
+            definition_id = line
+            province_id += id_line
+            #print(definition_id)
+    return province_id
+
+
+
+def position_file():
+    province_id = []
+    path = positions_file_path
+    with open(path, "r") as f:
+        for line in f:
+            if re.match(r"^\d+.*$", line):  # Searching for lines that start with a number
+                line = line.split('=', 1)[0].replace('\t', '').replace('\n', '').replace(' ', '')
+                temp_line = str(line).rstrip('\n').strip().split()
+                province_id += temp_line
+                #print(temp_line)
+    return province_id
+
+
 def terrain_file():
     province_id = []
     start = 'terrain_override = {'
@@ -130,14 +162,14 @@ def trade_nodes_file():
     return province_id
 
 
-def sea_or_lake():
+def sea_tile():
     global sea_or_lake_id
     start_sea = 'sea_starts = {'
-    start_lake = 'lakes = {'
+    #start_lake = 'lakes = {'
     end = '}'
     path = default_file_path
     found_type = False
-    with open(path, 'r', encoding='utf8' ) as f:
+    with open(path, 'r', encoding='utf8') as f:
         for line in f:
             if start_sea in line.strip():
                 found_type = True
@@ -152,9 +184,19 @@ def sea_or_lake():
                     line = line.split(comment, 1)[0]
                     temp_line = str(line).rstrip('\n').strip().split()
                     sea_id += temp_line
-                    #print(sea_or_lake_id)
-    with open(path, 'r', encoding='utf8' ) as f1:
-        for line1 in f1:
+                    sea_or_lake_id += sea_id
+    return sea_or_lake_id
+
+
+def lake_tile():
+    global sea_or_lake_id
+    #start_sea = 'sea_starts = {'
+    start_lake = 'lakes = {'
+    end = '}'
+    path = default_file_path
+    found_type = False
+    with open(path, 'r', encoding='utf8' ) as f:
+        for line1 in f:
             if start_lake in line1.strip():
                 found_type = True
                 continue
@@ -168,8 +210,8 @@ def sea_or_lake():
                     line1 = line1.split(comment, 1)[0]
                     temp_line1 = str(line1).rstrip('\n').strip().split()
                     lake_id += temp_line1
-                    sea_or_lake_id = lake_id + sea_id
-                    return sea_or_lake_id
+                    sea_or_lake_id += lake_id
+    return sea_or_lake_id
 
 
 def wasteland():
@@ -225,57 +267,68 @@ def province_history_files_name(province_history_path):
 # filtering Wasteland, Sea tiles and Lakes
 def filter_list(province_history_id):
     compare_list = []
-    compare_list.extend(sea_or_lake_id)
-    compare_list.extend(wasteland_id)
-    #compare_list = str(compare_list)
-    print(compare_list)
+    compare_list = sea_or_lake_id + wasteland_id
+    #print(compare_list)
     province_history_id = [x for x in province_history_id if x not in compare_list]
-    print(province_history_id)
+    #print(province_history_id)
     return province_history_id
 
 
-#
+# Missing id's
 def missing_province_id(province_history_id,province_id):
     missing_id = list(set(province_history_id).difference(province_id))
     return missing_id
 
 
+if default_present is True:
+    sea_tile()
+    lake_tile()
 if climate_present is True:
     wasteland()
-if default_present is True:
-    sea_or_lake()
 
 
 if province_history_present is True:
-    province_history_id = filter_list(province_history_files(province_history_path))
+    province_history_id = province_history_files(province_history_path)
     print("Loading File names from history/provinces/")
+    if definition_present is True:
+        province_id = definition_file()
+        duplicate = natural_sort(find_duplicate(province_id))
+        missing_province_id(province_history_id, province_id)
+        missing_id = natural_sort(missing_province_id(province_history_id, province_id))
+        print(f"Definition.csv Missing id:{missing_id}{os.linesep}Duplicate id:{duplicate}{os.linesep}")
+    if positions_present is True:
+        province_id = position_file()
+        duplicate = natural_sort(find_duplicate(province_id))
+        missing_province_id(province_history_id, province_id)
+        missing_id = natural_sort(missing_province_id(province_history_id, province_id))
+        print(f"Position.csv Missing id:{missing_id}{os.linesep}Duplicate id:{duplicate}{os.linesep}")
     if terrain_present is True:
         province_id = terrain_file()
         terrain_id = terrain_file()
         duplicate = natural_sort(find_duplicate(province_id))
         missing_province_id(province_history_id, province_id)
-        missing_id = natural_sort(missing_province_id(province_history_id, province_id))
+        missing_id = natural_sort(filter_list(missing_province_id(province_history_id, province_id)))
         print(f"Terrain.txt Missing id:{missing_id}{os.linesep}Duplicate id:{duplicate}{os.linesep}")
     if continent_present is True:
         province_id = continent_file()
         continent_id = continent_file()
         duplicate = natural_sort(find_duplicate(province_id))
         missing_province_id(province_history_id, province_id)
-        missing_id = natural_sort(missing_province_id(province_history_id, province_id))
+        missing_id = natural_sort(filter_list(missing_province_id(province_history_id, province_id)))
         print(f"Continent.txt Missing id: {missing_id}{os.linesep}Duplicate id:{duplicate}{os.linesep}")
     if area_present is True:
         province_id = area_file()
         area_id = area_file()
         duplicate = natural_sort(find_duplicate(province_id))
         missing_province_id(province_history_id,province_id)
-        missing_id = natural_sort(missing_province_id(province_history_id, province_id))
+        missing_id = natural_sort(filter_list(missing_province_id(province_history_id, province_id)))
         print(f"Area.txt Missing id: {missing_id}{os.linesep}Duplicate id:{duplicate}{os.linesep}")
     if trade_nodes_present is True:
         province_id = trade_nodes_file()
         trade_nodes_id = trade_nodes_file()
         duplicate = natural_sort(find_duplicate(province_id))
         missing_province_id(province_history_id, province_id)
-        missing_id = natural_sort(missing_province_id(province_history_id, province_id))
+        missing_id = natural_sort(filter_list(missing_province_id(province_history_id, province_id)))
         print(f"TradeNodes.txt Missing id: {missing_id}{os.linesep}Duplicate id:{duplicate}{os.linesep}")
     else:
         pass
@@ -305,7 +358,8 @@ if province_history_present is False:
 else:
     pass
 
-input_a = input("To print all history/province id's type 'id' or 'name'\nTo Save to a txt file type 'save id' or 'save name'\n")
+input_a = input("To print all history/province id's type 'id' or 'name'\nTo Save to a txt file type 'save id' or "
+                "'save name'\n")
 
 if input_a == "name":
     province_history_names = province_history_files_name(province_history_path)
